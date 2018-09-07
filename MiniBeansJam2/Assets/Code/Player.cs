655330e451d3d0afb8361b626b93388c39c4bc71
Tim Hagemann <tim@eternalwings.de>
2018-09-07 20:22:15 +0200

    Add basic pathfinding for zombie and player

--- MiniBeansJam2/Assets/Code/Player.cs
+++ MiniBeansJam2/Assets/Code/Player.cs
@@ -1,16 +1,31 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.AI;
 
-public class Player : MonoBehaviour {
-
+public class Player : MonoBehaviour
+{
+	public Vector3 CurrentTarget;
+	public Camera Camera;
+	private NavMeshAgent _agent;
+	
 	// Use this for initialization
-	void Start () {
-		
+	void Start ()
+	{
+		_agent = GetComponent<NavMeshAgent>();
 	}
 	
 	// Update is called once per frame
 	void Update () {
-		
+		if (!_agent.hasPath && Input.GetMouseButtonDown(0))
+		{
+			Ray clickRay = Camera.ScreenPointToRay(Input.mousePosition);
+			RaycastHit hit;
+			if (Physics.Raycast(clickRay, out hit))
+			{
+				CurrentTarget = hit.point;
+				_agent.SetDestination(CurrentTarget);
+			}
+		}
 	}
 }
---
+++ MiniBeansJam2/Assets/Code/Zombie.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+public class Zombie : MonoBehaviour
+{
+    public int MinWaitTime = 5;
+    public int MaxWaitTime = 10;
+    public List<Vector3> Waypoints;
+    public int CurrentWaypointIndex = 0;
+    public double WaitTime = 0;
+    public double NextWaypointStart = 3.0;
+    private Vector3 _currentWaypoint;
+    private NavMeshAgent _agent;
+
+    // Use this for initialization
+    void Start()
+    {
+        _agent = GetComponent<NavMeshAgent>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (!_agent.hasPath)
+        {
+            WaitTime += Time.deltaTime;
+            if (NextWaypointStart <= WaitTime)
+            {
+                CurrentWaypointIndex = (CurrentWaypointIndex + 1) % Waypoints.Count;
+                _currentWaypoint = Waypoints[CurrentWaypointIndex];
+                _agent.SetDestination(_currentWaypoint);
+                WaitTime = 0;
+                NextWaypointStart = Random.Range(MinWaitTime, MaxWaitTime);
+            }
+        }
+    }
+}
