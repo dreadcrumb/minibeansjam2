9c4c4fcdda22aee92b94ca491ab51172c25cfb1d
Tim Hagemann <tim@eternalwings.de>
2018-09-08 17:48:22 +0200

    Add hazard area to increase zombification

---
+++ MiniBeansJam2/Assets/Code/HazardArea.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections.Generic;
+using UnityEngine;
+
+public class HazardArea : MonoBehaviour
+{
+    public double ZombficationIncrementOnTouch = 0.01;
+    public bool ReapeatedTouch = false;
+
+    private HashSet<GameObject> _alreadyTouched = new HashSet<GameObject>();
+
+    private void OnTriggerEnter(Collider other)
+    {
+        var targetObject = other.gameObject;
+        if (targetObject.CompareTag("Player"))
+        {
+            if (ReapeatedTouch || !_alreadyTouched.Contains(targetObject))
+            {
+                var player = targetObject.GetComponent<Player>();
+                player.ZombificationPassiveIncrement += ZombficationIncrementOnTouch;
+                _alreadyTouched.Add(targetObject);
+            }
+        }
+    }
+}
