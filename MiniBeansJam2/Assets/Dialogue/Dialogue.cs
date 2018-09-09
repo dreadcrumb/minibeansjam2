14823da52f9172d66856c0b45bc50427981edb42
Tim Hagemann <tim@eternalwings.de>
2018-09-09 17:53:22 +0200

    Add dialog intro scene

---
+++ MiniBeansJam2/Assets/Dialogue/Dialogue.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[System.Serializable]
+public class Dialogue
+{
+
+	public Sprite person;
+	public string name;
+	[TextArea(2, 5)]
+	public string[] sentences;
+
+}
